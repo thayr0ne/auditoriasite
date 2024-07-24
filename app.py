4f006df7c6865809d140c463f4e379efb5c3dbd7
thayr0ne <57337073+thayr0ne@users.noreply.github.com>
2024-07-24 13:21:03 -0300

    Update app.py

--- app.py
+++ app.py
@@ -23,26 +23,43 @@ def fetch_ans_links():
             href = link.get('href')
             texto = link.get_text().strip()
             if 'Alterado pela RN' in texto:
-                rn_match = re.search(r'RN nº (\d+), de', texto)
+                rn_match = re.search(r'RN nº (\d+), de (\d{2}/\d{2}/\d{4})', texto)
                 if rn_match:
                     rn_num = int(rn_match.group(1))
-                    rn_links.append((rn_num, texto, href))
+                    rn_date = rn_match.group(2)
+                    rn_links.append((rn_num, rn_date, texto, href))
             elif 'ANEXO II' in texto and href.endswith('.pdf'):
-                anexo_ii_links.append(href)
+                anexo_ii_links.append((texto, href))
         
-        latest_rn_link = ""
+        latest_rn = {
+            'number': None,
+            'date': None,
+            'text': None,
+            'link': None
+        }
         if rn_links:
             rn_links.sort(reverse=True, key=lambda x: x[0])
-            _, _, rn_href = rn_links[0]
-            latest_rn_link = urljoin(url, rn_href)
+            rn_num, rn_date, rn_text, rn_href = rn_links[0]
+            latest_rn = {
+                'number': rn_num,
+                'date': rn_date,
+                'text': rn_text,
+                'link': urljoin(url, rn_href)
+            }
 
-        latest_anexo_ii_link = ""
+        latest_anexo_ii = {
+            'text': None,
+            'link': None
+        }
         if anexo_ii_links:
-            latest_anexo_ii_link = urljoin(url, anexo_ii_links[0])
+            latest_anexo_ii = {
+                'text': anexo_ii_links[0][0],
+                'link': urljoin(url, anexo_ii_links[0][1])
+            }
 
         return jsonify({
-            'latest_rn_link': latest_rn_link,
-            'latest_anexo_ii_link': latest_anexo_ii_link
+            'latest_rn': latest_rn,
+            'latest_anexo_ii': latest_anexo_ii
         })
     else:
         return jsonify({
