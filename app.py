ddbb901c9a62bd82055a3247e6fddafd593c7245
thayr0ne <57337073+thayr0ne@users.noreply.github.com>
2024-07-25 12:32:28 -0300

    Update app.py

--- app.py
+++ app.py
@@ -12,11 +12,11 @@ CORS(app, resources={r"/api/*": {"origins": "*"}})
 def fetch_ans_links():
     url = 'https://www.ans.gov.br/component/legislacao/?view=legislacao&task=TextoLei&format=raw&id=NDAzMw==#anexosvigentes'
     response = requests.get(url)
-
+    
     if response.status_code == 200:
         soup = BeautifulSoup(response.content, 'html.parser')
         links = soup.find_all('a')
-
+        
         rn_links = []
         anexo_ii_links = []
         for link in links:
@@ -29,7 +29,7 @@ def fetch_ans_links():
                     rn_links.append((rn_num, texto, href))
             elif 'ANEXO II' in texto and href.endswith('.pdf'):
                 anexo_ii_links.append((texto, href))
-
+        
         rn_links.sort(reverse=True, key=lambda x: x[0])
         latest_rn_links = []
         for rn_num, texto, href in rn_links:
@@ -72,8 +72,12 @@ def fetch_rn_summary():
 
     if response.status_code == 200:
         soup = BeautifulSoup(response.content, 'html.parser')
-        summary = soup.find('p', align='right').get_text().strip()
-        return jsonify({'summary': summary})
+        summary = soup.find('p', align='right')
+        if summary:
+            summary_text = summary.get_text().strip()
+            return jsonify({'summary': summary_text})
+        else:
+            return jsonify({'error': 'Resumo não encontrado'}), 404
     else:
         return jsonify({'error': 'Erro ao acessar a RN'}), 500
 
