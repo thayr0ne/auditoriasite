fa32eeece44dffb0b677cecb89667620db1b1936
thayr0ne <57337073+thayr0ne@users.noreply.github.com>
2024-07-24 23:56:15 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -38,40 +38,38 @@ document.addEventListener('DOMContentLoaded', function() {
     showSection('anexosVigentes');
 
     // Lógica para buscar links do backend
-    document.getElementById('fetchLinksBtn').addEventListener('click', function() {
-        fetch('https://auditoriasite.vercel.app/api/fetch-ans-links')
-            .then(response => response.json())
-            .then(data => {
-                const latestAnexoIIContainer = document.getElementById('latestAnexoIIContainer');
-                const latestRnContainer = document.getElementById('latestRnContainer');
+    fetch('https://auditoriasite.vercel.app/api/fetch-ans-links')
+        .then(response => response.json())
+        .then(data => {
+            const latestAnexoIIContainer = document.getElementById('latestAnexoIIContainer');
+            const latestRnContainer = document.getElementById('latestRnContainer');
 
-                latestAnexoIIContainer.innerHTML = `
-                    <h2>Recentes</h2>
-                    <div class="link-item">
-                        <strong>Anexo II - Modificado em ${formatDate(data.latest_anexo_ii_date)}</strong>
-                        <button onclick="viewPDF('${data.latest_anexo_ii_link}')">Exibir</button>
-                        <button onclick="downloadPDF('${data.latest_anexo_ii_link}')">Download</button>
-                    </div>
+            latestAnexoIIContainer.innerHTML = `
+                <h2>Recentes</h2>
+                <div class="link-item">
+                    <strong>Anexo II - Modificado em ${formatDate(data.latest_anexo_ii_date)}</strong>
+                    <button onclick="viewPDF('${data.latest_anexo_ii_link}')">Exibir</button>
+                    <button onclick="downloadPDF('${data.latest_anexo_ii_link}')">Download</button>
+                </div>
+            `;
+
+            if (Array.isArray(data.latest_rn_links)) {
+                latestRnContainer.innerHTML = `
+                    <h2>Histórico</h2>
+                    ${data.latest_rn_links.map(link => `
+                        <div class="link-item">
+                            <strong>RN nº ${link.number} (${formatDate(link.date)})</strong>
+                            <button onclick="viewPDF('${link.url}')">Exibir</button>
+                        </div>
+                    `).join('')}
                 `;
-
-                if (Array.isArray(data.latest_rn_links)) {
-                    latestRnContainer.innerHTML = `
-                        <h2>Histórico</h2>
-                        ${data.latest_rn_links.map(link => `
-                            <div class="link-item">
-                                <strong>RN nº ${link.number} (${formatDate(link.date)})</strong>
-                                <button onclick="viewPDF('${link.url}')">Exibir</button>
-                            </div>
-                        `).join('')}
-                    `;
-                } else {
-                    latestRnContainer.innerHTML = '<h2>Histórico</h2><p>Nenhuma RN encontrada</p>';
-                }
-            })
-            .catch(error => {
-                alert('Erro ao obter os links: ' + error);
-            });
-    });
+            } else {
+                latestRnContainer.innerHTML = '<h2>Histórico</h2><p>Nenhuma RN encontrada</p>';
+            }
+        })
+        .catch(error => {
+            alert('Erro ao obter os links: ' + error);
+        });
 
     window.viewPDF = function(link) {
         document.getElementById('pdfViewer').src = link;
