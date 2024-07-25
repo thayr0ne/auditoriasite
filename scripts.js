b921b7557db77319b91dedabd7b2c62886f62f31
thayr0ne <57337073+thayr0ne@users.noreply.github.com>
2024-07-24 23:09:32 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -46,13 +46,20 @@ document.addEventListener('DOMContentLoaded', function() {
                 linksContainer.innerHTML = `
                     <div class="link-item">
                         <strong>Link da RN mais recente:</strong> 
-                        <a href="${data.latest_rn_link}" target="_blank">${data.latest_rn_link}</a>
+                        <a href="#" data-link="${data.latest_rn_link}" class="link">RN mais recente</a>
                     </div>
                     <div class="link-item">
                         <strong>Link do Anexo II mais recente:</strong> 
-                        <a href="${data.latest_anexo_ii_link}" target="_blank">${data.latest_anexo_ii_link}</a>
+                        <a href="#" data-link="${data.latest_anexo_ii_link}" class="link">Anexo II mais recente</a>
                     </div>
                 `;
+                document.querySelectorAll('.link').forEach(link => {
+                    link.addEventListener('click', function(event) {
+                        event.preventDefault();
+                        const linkUrl = event.target.getAttribute('data-link');
+                        document.getElementById('pdfViewer').src = linkUrl;
+                    });
+                });
             })
             .catch(error => {
                 alert('Erro ao obter os links: ' + error);
