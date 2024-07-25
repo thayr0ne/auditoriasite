75505266c7b6bbfb58d1a2bf4628da8e2602e03e
thayr0ne <57337073+thayr0ne@users.noreply.github.com>
2024-07-25 12:16:21 -0300

    Update app.py

--- app.py
+++ app.py
@@ -6,17 +6,17 @@ from urllib.parse import urljoin
 import re
 
 app = Flask(__name__)
-CORS(app, resources={r"/api/*": {"origins": "*"}})  # Permitir todas as origens
+CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 @app.route('/api/fetch-ans-links')
 def fetch_ans_links():
     url = 'https://www.ans.gov.br/component/legislacao/?view=legislacao&task=TextoLei&format=raw&id=NDAzMw==#anexosvigentes'
     response = requests.get(url)
-    
+
     if response.status_code == 200:
         soup = BeautifulSoup(response.content, 'html.parser')
         links = soup.find_all('a')
-        
+
         rn_links = []
         anexo_ii_links = []
         for link in links:
@@ -29,7 +29,7 @@ def fetch_ans_links():
                     rn_links.append((rn_num, texto, href))
             elif 'ANEXO II' in texto and href.endswith('.pdf'):
                 anexo_ii_links.append((texto, href))
-        
+
         rn_links.sort(reverse=True, key=lambda x: x[0])
         latest_rn_links = []
         for rn_num, texto, href in rn_links:
@@ -64,12 +64,12 @@ def fetch_ans_links():
 def fetch_rn_summary():
     data = request.get_json()
     rn_url = data.get('url')
-    
+
     if not rn_url:
         return jsonify({'error': 'URL da RN não fornecida'}), 400
-    
+
     response = requests.get(rn_url)
-    
+
     if response.status_code == 200:
         soup = BeautifulSoup(response.content, 'html.parser')
         summary = soup.find('p', align='right').get_text().strip()
