c061dffe2e7fb7f883247768080823f6fbec7a9d
thayr0ne <57337073+thayr0ne@users.noreply.github.com>
2024-07-25 11:57:13 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 import requests
 from bs4 import BeautifulSoup
@@ -28,7 +28,7 @@ def fetch_ans_links():
                     rn_num = int(rn_match.group(1))
                     rn_links.append((rn_num, texto, href))
             elif 'ANEXO II' in texto and href.endswith('.pdf'):
-                anexo_ii_links.append(href)
+                anexo_ii_links.append((texto, href))
         
         rn_links.sort(reverse=True, key=lambda x: x[0])
         latest_rn_links = []
@@ -45,8 +45,10 @@ def fetch_ans_links():
         latest_anexo_ii_link = ""
         latest_anexo_ii_date = ""
         if anexo_ii_links:
-            latest_anexo_ii_link = urljoin(url, anexo_ii_links[0])
-            latest_anexo_ii_date = rn_links[0][1].split('de')[1].strip()  # Extrair a data correta do texto do link
+            latest_anexo_ii_link = urljoin(url, anexo_ii_links[0][1])
+            latest_anexo_ii_date_match = re.search(r'(\d{2}/\d{2}/\d{4})', anexo_ii_links[0][0])
+            if latest_anexo_ii_date_match:
+                latest_anexo_ii_date = latest_anexo_ii_date_match.group(1)
 
         return jsonify({
             'latest_rn_links': latest_rn_links,
@@ -57,6 +59,23 @@ def fetch_ans_links():
         return jsonify({
             'error': 'Erro ao acessar a página'
         })
+
+@app.route('/api/fetch-rn-summary', methods=['POST'])
+def fetch_rn_summary():
+    data = request.get_json()
+    rn_url = data.get('url')
+    
+    if not rn_url:
+        return jsonify({'error': 'URL da RN não fornecida'}), 400
+    
+    response = requests.get(rn_url)
+    
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, 'html.parser')
+        summary = soup.find('p', align='right').get_text().strip()
+        return jsonify({'summary': summary})
+    else:
+        return jsonify({'error': 'Erro ao acessar a RN'}), 500
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
