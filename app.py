430e444b456f4cb1b0eafe5d823bb3b9bcd77612
thayr0ne <57337073+thayr0ne@users.noreply.github.com>
2024-07-25 00:50:30 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify
 from flask_cors import CORS
 import requests
 from bs4 import BeautifulSoup
+import re
 
 app = Flask(__name__)
 CORS(app)
@@ -23,12 +24,12 @@ def fetch_ans_links():
             text = link.get_text(strip=True)
             href = link.get('href')
             if 'Anexo II' in text and href:
-                latest_anexo_ii_link = f"https://www.ans.gov.br{href.replace('../../..', '')}"
+                latest_anexo_ii_link = complete_url(href)
                 latest_anexo_ii_date = extract_date_from_text(text)
 
             if 'Alterado pela RN' in text and href:
                 latest_rn_links.append({
-                    'url': f"https://www.ans.gov.br{href.replace('../../..', '')}",
+                    'url': complete_url(href),
                     'number': extract_rn_number_from_text(text),
                     'date': extract_date_from_text(text)
                 })
@@ -47,14 +48,17 @@ def fetch_ans_links():
         return jsonify({'error': 'Erro ao acessar a página da ANS'}), 500
 
 def extract_date_from_text(text):
-    import re
     match = re.search(r'\d{2}/\d{2}/\d{4}', text)
     return match.group(0) if match else None
 
 def extract_rn_number_from_text(text):
-    import re
     match = re.search(r'\d+', text)
     return match.group(0) if match else None
+
+def complete_url(href):
+    if href.startswith('http'):
+        return href
+    return f"https://www.ans.gov.br{href.replace('../../..', '')}"
 
 if __name__ == '__main__':
     app.run()
