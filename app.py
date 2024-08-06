6f2283a9579c7642e2eb002d8ea364e9244b9d39
thayr0ne <57337073+thayr0ne@users.noreply.github.com>
2024-08-06 19:27:56 -0300

    Update app.py
    com resumos atualizados

--- app.py
+++ app.py
@@ -4,37 +4,21 @@ import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 import re
-import logging
 
 app = Flask(__name__)
 CORS(app, resources={r"/api/*": {"origins": "*"}})  # Permitir todas as origens
 
-# Configurar logging
-logging.basicConfig(level=logging.INFO)
-
 @app.route('/api/fetch-ans-links', methods=['GET'])
 def fetch_ans_links():
     url = 'https://www.ans.gov.br/component/legislacao/?view=legislacao&task=TextoLei&format=raw&id=NDAzMw==#anexosvigentes'
-    logging.info(f'Fetching data from URL: {url}')
-    try:
-        response = requests.get(url)
-    except requests.exceptions.RequestException as e:
-        logging.error(f'Error fetching data from URL: {e}')
-        return jsonify({'error': f'Error fetching data from URL: {e}'}), 500
+    response = requests.get(url)
     
     if response.status_code == 200:
-        logging.info('Page fetched successfully')
         soup = BeautifulSoup(response.content, 'html.parser')
         links = soup.find_all('a')
-        logging.info(f'Found {len(links)} links on the page')
         
         rn_links = []
-        anexo_links = {
-            'I': [],
-            'II': [],
-            'III': [],
-            'IV': []
-        }
+        anexo_links = {'I': '', 'II': '', 'III': '', 'IV': ''}
         for link in links:
             href = link.get('href')
             texto = link.get_text().strip()
@@ -45,11 +29,9 @@ def fetch_ans_links():
                     rn_links.append((rn_num, texto, href))
             for anexo in anexo_links.keys():
                 if f'ANEXO {anexo}' in texto and href.endswith('.pdf'):
-                    anexo_links[anexo].append((texto, href))
+                    anexo_links[anexo] = urljoin(url, href)
         
-        rn_links = list(dict.fromkeys(rn_links))  # Remove duplicatas
         rn_links.sort(reverse=True, key=lambda x: x[0])
-        logging.info(f'Sorted and deduplicated RN links: {rn_links}')
         latest_rn_links = []
         for rn_num, texto, href in rn_links:
             date_match = re.search(r'de (\d{2}/\d{2}/\d{4})', texto)
@@ -61,24 +43,11 @@ def fetch_ans_links():
                     'url': urljoin(url, href)
                 })
 
-        latest_anexo_links = {}
-        for anexo, links in anexo_links.items():
-            if links:
-                latest_anexo_text, latest_anexo_href = links[0]
-                latest_anexo_links[anexo] = urljoin(url, latest_anexo_href)
-                logging.info(f'Latest Anexo {anexo} text: {latest_anexo_text}')
-                logging.info(f'Latest Anexo {anexo} href: {latest_anexo_href}')
-            else:
-                latest_anexo_links[anexo] = ""
-
-        logging.info(f'Latest Anexo links: {latest_anexo_links}')
-
         return jsonify({
             'latest_rn_links': latest_rn_links,
-            'latest_anexo_links': latest_anexo_links
+            'latest_anexo_links': anexo_links
         })
     else:
-        logging.error(f'Error fetching page: {response.status_code}')
         return jsonify({
             'error': 'Erro ao acessar a página'
         })
@@ -87,33 +56,16 @@ def fetch_ans_links():
 def fetch_rn_summary():
     data = request.get_json()
     url = data.get('url')
-    logging.info(f'Fetching summary for URL: {url}')
     if not url:
-        logging.error('URL não fornecida')
         return jsonify({'error': 'URL não fornecida'}), 400
 
-    try:
-        response = requests.get(url)
-    except requests.exceptions.RequestException as e:
-        logging.error(f'Error fetching data from URL: {e}')
-        return jsonify({'error': f'Error fetching data from URL: {e}'}), 500
-    
+    response = requests.get(url)
     if response.status_code == 200:
         soup = BeautifulSoup(response.content, 'html.parser')
-        paragraphs = soup.find_all('p', align='right')
-        logging.info(f'Found {len(paragraphs)} paragraphs with right alignment')
+        paragraphs = soup.find_all('p', class_='ementa')
         summary = "\n".join(p.get_text().strip() for p in paragraphs)
-        if not summary:
-            first_paragraph = soup.find('p')
-            if first_paragraph:
-                summary = first_paragraph.get_text().strip()
-                logging.info(f'Using first paragraph as summary: {summary}')
-            else:
-                logging.warning('No summary found')
-        logging.info(f'Summary found: {summary}')
         return jsonify({'summary': summary})
     else:
-        logging.error(f'Error fetching summary: {response.status_code}')
         return jsonify({'error': 'Erro ao acessar a página da RN'}), 500
 
 if __name__ == '__main__':
