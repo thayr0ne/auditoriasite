a939839daeada8d87ccbb19691761f566d2cbe6b
thayr0ne <57337073+thayr0ne@users.noreply.github.com>
2024-08-08 18:37:39 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -140,8 +140,8 @@ document.addEventListener('DOMContentLoaded', function() {
             .then(data => {
                 if (data.excel_url) {
                     console.log('URL do Excel:', data.excel_url);
-                    const excelViewerContainer = document.getElementById('excelViewerContainer');
-                    excelViewerContainer.innerHTML = `<iframe src="https://view.officeapps.live.com/op/embed.aspx?src=${data.excel_url}" width="100%" height="100%" frameborder="0"></iframe>`;
+                    const excelViewer = document.getElementById('excelViewer');
+                    excelViewer.src = `https://view.officeapps.live.com/op/embed.aspx?src=${data.excel_url}`;
                 } else {
                     console.error('Erro ao obter o URL do Excel:', data.error);
                     alert('Erro ao obter o URL do Excel: ' + (data.error || 'Erro desconhecido'));
