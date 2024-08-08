bfb42c4745e45fc96db53ffec8695a3c4f3428d2
thayr0ne <57337073+thayr0ne@users.noreply.github.com>
2024-08-08 17:12:55 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,4 +1,3 @@
-
 document.addEventListener('DOMContentLoaded', function() {
     const sections = {
         anexosVigentes: document.getElementById('anexosVigentes'),
@@ -134,19 +133,24 @@ document.addEventListener('DOMContentLoaded', function() {
             console.error('Erro ao obter os links:', error);
             alert('Erro ao obter os links: ' + error);
         });
- function fetchRolVigente() {
-        fetch('https://auditoriasite.onrender.com/api/fetch-limited-rol')
-            .then(response => response.blob())
-            .then(blob => {
-                const url = URL.createObjectURL(blob);
-                const excelViewerContainer = document.getElementById('excelViewerContainer');
-                excelViewerContainer.innerHTML = `<iframe src="https://view.officeapps.live.com/op/embed.aspx?src=${encodeURIComponent(url)}" width="100%" height="100%" frameborder="0"></iframe>`;
+
+    function fetchRolVigente() {
+        fetch('https://auditoriasite.onrender.com/api/fetch-rol-vigente')
+            .then(response => response.json())
+            .then(data => {
+                if (data.excel_url) {
+                    console.log('URL do Excel:', data.excel_url);
+                    const excelViewerContainer = document.getElementById('excelViewerContainer');
+                    excelViewerContainer.innerHTML = `<iframe src="https://view.officeapps.live.com/op/embed.aspx?src=${data.excel_url}" width="100%" height="100%" frameborder="0"></iframe>`;
+                } else {
+                    console.error('Erro ao obter o URL do Excel:', data.error);
+                    alert('Erro ao obter o URL do Excel: ' + (data.error || 'Erro desconhecido'));
+                }
             })
             .catch(error => {
                 console.error('Erro ao obter o URL do Excel:', error);
                 alert('Erro ao obter o URL do Excel: ' + error);
             });
-    
     }
 
     window.viewPDF = function(link) {
